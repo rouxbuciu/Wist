8a2ecb2ddcecbbb223a49579fa2b770c321e06d0
roux g. buciu <roux@acmelabs.ca>
2016-10-22 12:58:39 -0400

    Added relationships, generated files and finised data model in Core Data

---
+++ Wist/Wist/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  Wist
+//
+//  Created by roux g. buciu on 2016-10-22.
+//  Copyright © 2016 roux g. buciu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
---
+++ Wist/Wist/Store+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Store+CoreDataClass.swift
+//  Wist
+//
+//  Created by roux g. buciu on 2016-10-22.
+//  Copyright © 2016 roux g. buciu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Store: NSManagedObject {
+
+}
